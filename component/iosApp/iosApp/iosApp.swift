a88872a1a0d4f9715f8ed65b6a69e65663fe8ffd
Pablo Valdes <am.pablo.vc@gmail.com>
2023-03-24 04:29:57 -0400

    - Add some logs in the swift code to print iOS Lifecycle stuff

--- component/iosApp/iosApp/iosApp.swift
+++ component/iosApp/iosApp/iosApp.swift
@@ -4,6 +4,7 @@ import ComponentKt
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var iosBridgeCopy: ComponentIosBridge?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
               "bottom:\(safeAreaInsets.bottom)"
         )
         let iosBridge = BindingsKt.createPlatformBridge()
+        iosBridgeCopy = iosBridge
         
         let left = Int32(safeAreaInsets.left.rounded())
         let top = Int32(safeAreaInsets.top.rounded())
@@ -36,11 +38,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
-    
-        iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
-            appLifecycleEvent: .Start
-        )
-        
         return true
     }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("applicationWillEnterForeground")
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("applicationDidBecomeActive")
+        iosBridgeCopy?.appLifecycleDispatcher.dispatchAppLifecycleEvent(appLifecycleEvent: .Start)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("applicationWillResignActive")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("applicationDidEnterBackground")
+        iosBridgeCopy?.appLifecycleDispatcher.dispatchAppLifecycleEvent(appLifecycleEvent: .Stop)
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("applicationWillTerminate")
+    }
+    
 }
