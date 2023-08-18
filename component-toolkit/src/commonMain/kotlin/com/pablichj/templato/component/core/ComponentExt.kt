ddbadb8a9a29c1eadb7ab334b61b7d4280ed03b5
Pablo Valdes <am.pablo.vc@gmail.com>
2023-08-18 17:49:22 -0400

    - Add repeatOnLifecycle, collectAsStateWithLifecycle extension functions.

--- component-toolkit/src/commonMain/kotlin/com/pablichj/templato/component/core/ComponentExt.kt
+++ component-toolkit/src/commonMain/kotlin/com/pablichj/templato/component/core/ComponentExt.kt
@@ -1,7 +1,19 @@
 package com.pablichj.templato.component.core
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
 import com.pablichj.templato.component.core.backpress.BackPressHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun Component.getFirstParentMatching(
     condition: (Component) -> Boolean
@@ -46,3 +58,81 @@ fun Component.jsonify(): JsonObject {
     return rootJsonObject
 }
 */
+
+suspend fun Component.repeatOnLifecycle(
+    block: suspend CoroutineScope.() -> Unit
+) {
+
+    if (lifecycleState === ComponentLifecycleState.Destroyed) {
+        return
+    }
+
+    // This scope is required to preserve context before we move to Dispatchers.Main
+    coroutineScope {
+        withContext(Dispatchers.Main.immediate) {
+            // Check the current state of the lifecycle as the previous check is not guaranteed
+            // to be done on the main thread.
+            if (lifecycleState === ComponentLifecycleState.Destroyed) return@withContext
+
+            // Instance of the running repeating coroutine
+            var launchedJob: Job? = null
+
+            try {
+                lifecycleStateFlow.collect { componentLifecycleState ->
+                    when (componentLifecycleState) {
+                        ComponentLifecycleState.Created,
+                        ComponentLifecycleState.Destroyed -> {
+                            // no-op
+                        }
+
+                        ComponentLifecycleState.Started -> {
+                            // Launch the repeating work preserving the calling context
+                            launchedJob = this@coroutineScope.launch {
+                                // coroutineScope ensures all child coroutines finish
+                                coroutineScope {
+                                    block()
+                                }
+                            }
+                        }
+
+                        ComponentLifecycleState.Stopped -> {
+                            launchedJob?.cancel()
+                            launchedJob = null
+                        }
+                    }
+
+                }
+            } finally {
+                launchedJob?.cancel()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun <T> Flow<T>.collectAsStateWithLifecycle(
+    initialValue: T,
+    component: Component,
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> {
+    return produceState(initialValue, this, component, context) {
+        component.repeatOnLifecycle {
+            if (context == EmptyCoroutineContext) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            } else withContext(context) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            }
+        }
+    }
+}
+
+@Composable
+fun <T> StateFlow<T>.collectAsStateWithLifecycle(
+    component: Component,
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> = collectAsStateWithLifecycle(
+    initialValue = this.value,
+    component = component,
+    context = context
+)
