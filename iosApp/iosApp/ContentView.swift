6d95c750146648041fbb906c6651938abcf71176
pablichjenkov <am.pablo.vc@gmail.com>
2023-08-18 04:10:34 -0400

    - Migrate iosApp to swiftui (#169)

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import UIKit
+import ComponentDemoKt
+
+struct ComposeView : UIViewControllerRepresentable {
+
+    var iosBridge: IosBridge
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let drawerComponent = BindingsKt.buildDrawerComponent()
+        // let adaptableComponent = BindingsKt.buildAdaptableSizeComponent()
+        // let pagerComponent = BindingsKt.buildPagerComponent()
+        
+        let mainViewController = BindingsKt.ComponentRenderer(
+            rootComponent: drawerComponent,
+            iosBridge: iosBridge
+        )
+
+        return mainViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+
+    var iosBridge: IosBridge
+
+    var body: some View {
+        ComposeView(iosBridge: iosBridge)
+                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+                //.ignoresSafeArea(.all, edges: .bottom) // If prefered to handle this in compose land
+
+    }
+}
--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -1,53 +1,37 @@
-import UIKit
+import SwiftUI
 import ComponentDemoKt
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    
-    let iosBridge = IosBridge(
-        appLifecycleDispatcher: DefaultAppLifecycleDispatcher()
-    )
+@main
+struct iOSDemoAppApp: App {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-    
-        let drawerComponent = BindingsKt.buildDrawerComponent()
-        let adaptableComponent = BindingsKt.buildAdaptableSizeComponent()
-        let pagerComponent = BindingsKt.buildPagerComponent()
+    let iosBridge = IosBridge(appLifecycleDispatcher: DefaultAppLifecycleDispatcher())
+
+    var body: some Scene {
+       WindowGroup {
+           ZStack {
+               Color.white.ignoresSafeArea(.all) // status bar color
+               ContentView(iosBridge: iosBridge)
+                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                       print("application_willEnterForeground")
+                   }
+                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                       print("application_didBecomeActive")
+                       iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
+                           appLifecycleEvent: .start
+                       )
+                   }
+                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                       print("application_willResignActive")
+                   }.onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
+                       print("application_didEnterBackground")
+                       iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
+                           appLifecycleEvent: .stop
+                       )
+                   }
+           }.preferredColorScheme(.light)
+
+       }
         
-        let mainViewController = BindingsKt.ComponentRenderer(
-            rootComponent: drawerComponent,
-            iosBridge: iosBridge
-        )
-        
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
-        return true
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        print("applicationWillEnterForeground")
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        print("applicationDidBecomeActive")
-        iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
-            appLifecycleEvent: .start
-        )
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        print("applicationWillResignActive")
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        print("applicationDidEnterBackground")
-        iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(appLifecycleEvent: .stop)
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
-    }
-    
+   }
+
 }
