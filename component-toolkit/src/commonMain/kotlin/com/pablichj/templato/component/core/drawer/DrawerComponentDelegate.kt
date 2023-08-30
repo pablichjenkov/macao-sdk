a00ce3676551cf2342892b44cd05825eb13f1945
Pablo Valdes <am.pablo.vc@gmail.com>
2023-08-29 23:59:27 -0400

    - cleanup

--- component-toolkit/src/commonMain/kotlin/com/pablichj/templato/component/core/drawer/DrawerComponentDelegate.kt
+++ component-toolkit/src/commonMain/kotlin/com/pablichj/templato/component/core/drawer/DrawerComponentDelegate.kt
@@ -2,13 +2,14 @@ package com.pablichj.templato.component.core.drawer
 
 import com.pablichj.templato.component.core.Component
 import com.pablichj.templato.component.core.NavigationComponent
-import com.pablichj.templato.component.core.NavigationComponentDefaultLifecycleHandler
+import com.pablichj.templato.component.core.NavigationComponentDefaults
 import com.pablichj.templato.component.core.stack.AddAllPushStrategy
 import com.pablichj.templato.component.core.stack.PushStrategy
 import com.pablichj.templato.component.platform.CoroutineDispatchers
 
 abstract class DrawerComponentDelegate<T : DrawerStatePresenter>(
-    private val lifecycleHandler: NavigationComponent.LifecycleHandler = NavigationComponentDefaultLifecycleHandler()
+    private val lifecycleHandler: NavigationComponent.LifecycleHandler =
+        NavigationComponentDefaults.createLifecycleHandler()
 ) : NavigationComponent.LifecycleHandler by lifecycleHandler {
 
     val dispatchers: CoroutineDispatchers = CoroutineDispatchers.Defaults
