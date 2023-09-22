9e8c2c225fcf65262c5285f132e90f6235b1cbca
Pablo Valdes <am.pablo.vc@gmail.com>
2023-09-22 11:21:59 -0400

    - Add support to x64 architecture.

--- component-toolkit/build.gradle.kts
+++ component-toolkit/build.gradle.kts
@@ -171,6 +171,7 @@ kotlin {
 
     // IOS
     listOf(
+        iosX64(),
         iosArm64(),
         iosSimulatorArm64(),
     ).forEach { target ->
@@ -250,17 +251,23 @@ kotlin {
         val androidInstrumentedTest by getting
 
         // IOS
+        val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
+
         val iosMain by creating {
             dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
+
+        val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
