1520fee326cd03d5bc676627eaa1010e40114247
pablichjenkov <am.pablo.vc@gmail.com>
2023-11-05 23:49:16 -0500

    - Migrate gradle files to the new default hierarchy. (#292)

--- jsApp/build.gradle.kts
+++ jsApp/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 compose {
     // Sets a specific JetBrains Compose Compiler version
-/*
-    kotlinCompilerPlugin.set("1.4.7")
-    // Sets a specific Google Compose Compiler version
-    // kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.2")
-    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.8.21")
-*/
+    /*
+        kotlinCompilerPlugin.set("1.4.7")
+        // Sets a specific Google Compose Compiler version
+        // kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.2")
+        kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.8.21")
+    */
 
     experimental {
         web.application {}
@@ -24,17 +24,15 @@ kotlin {
         binaries.executable()
     }
     sourceSets {
-        val jsMain by getting  {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.ui)
-                implementation(compose.material3)
-                implementation(project(":shared"))
-                implementation(project(":component-toolkit"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.7.3")
-            }
+        jsMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(compose.material3)
+            implementation(project(":shared"))
+            implementation(project(":component-toolkit"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.7.3")
         }
     }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,9 +15,10 @@ version = extra["component-toolkit.version"] as String
 
 kotlin {
     applyDefaultHierarchyTemplate()
+
     // IOS
     listOf(
-        // iosX64(),
+        iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
@@ -38,39 +39,17 @@ kotlin {
     jvm()
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.ui)
-                implementation(compose.material3)
-                implementation(project(":component-toolkit"))
-                implementation("org.jetbrains.compose.components:components-resources:1.5.1")
-            }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(compose.material3)
+            implementation(project(":component-toolkit"))
+            implementation("org.jetbrains.compose.components:components-resources:1.5.10")
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
-
-        // IOS
-/*
-        val iosArm64Main by getting
-        //val iosSimulatorArm64Main by getting
-        val iosMain by getting {
-            dependsOn(commonMain)
-            iosArm64Main.dependsOn(this)
-         //   iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosArm64Test by getting
-//        val iosSimulatorArm64Test by getting
-        val iosTest by getting {
-            dependsOn(commonTest)
-            iosArm64Test.dependsOn(this)
-//            iosSimulatorArm64Test.dependsOn(this)
-        }
-*/
     }
 
 }
