9dfd5f72deba7ce1d2dc7eaad4729da90bd3f2ab
Pablo Valdes <am.pablo.vc@gmail.com>
2024-01-31 00:42:58 -0500

    Cleanup some unnecessary Kotlin extensions.

--- macao-sdk-di-manual/src/iosMain/kotlin/com/macaosoftware/app/MacaoApplication.kt
+++ macao-sdk-di-manual/src/iosMain/kotlin/com/macaosoftware/app/MacaoApplication.kt
@@ -2,25 +2,22 @@ package com.macaosoftware.app
 
 import androidx.compose.runtime.Composable
 import com.macaosoftware.component.IosComponentRender
-import com.macaosoftware.plugin.IosBridge
-import com.macaosoftware.util.elseIfNull
-import com.macaosoftware.util.ifNotNull
 
 @Composable
 fun MacaoApplication(
     onBackPress: () -> Unit,
-    macaoApplicationState: MacaoApplicationState,
-    splashScreenContent: @Composable () -> Unit
+    macaoApplicationState: MacaoApplicationState
 ) {
 
-    val rootComponent = macaoApplicationState.rootComponentState.value
-    rootComponent.ifNotNull {
-        IosComponentRender(
-            rootComponent = it,
-            onBackPress = onBackPress
-        )
-    }.elseIfNull {
-        splashScreenContent()
-        macaoApplicationState.fetchRootComponent()
-    }
+    macaoApplicationState.rootComponentState.value
+        .takeIf { it != null }
+        ?.also {
+            IosComponentRender(
+                rootComponent = it,
+                onBackPress = onBackPress
+            )
+        }
+        ?: {
+            macaoApplicationState.fetchRootComponent()
+        }
 }
