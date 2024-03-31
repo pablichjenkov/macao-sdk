ed109d7ba1cf2e55758c8ba611dafc0d3b418a92
Pablo Valdes <am.pablo.vc@gmail.com>
2024-03-30 20:28:25 -0400

    Bump gradle

--- component-toolkit/src/commonMain/kotlin/com/macaosoftware/component/viewmodel/ComponentViewModel.kt
+++ component-toolkit/src/commonMain/kotlin/com/macaosoftware/component/viewmodel/ComponentViewModel.kt
@@ -44,4 +44,8 @@ abstract class ComponentViewModel : ComponentViewModelLifecycle() {
         return false
     }
 
+    open fun instanceId(): String {
+        val addressLast5 = this.toString().let { it.substring(it.length - 5) }
+        return "${this::class.simpleName}_${addressLast5}"
+    }
 }
