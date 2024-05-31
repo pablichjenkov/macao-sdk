638c65dadbdc649b85b342fdca32557599f89149
Pablo Valdes <am.pablo.vc@gmail.com>
2024-05-31 02:11:09 -0400

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,8 @@ plugins {
     kotlin("multiplatform") apply false
     id("org.jetbrains.kotlin.plugin.compose") apply false
     id("org.jetbrains.compose") apply false
-    kotlin("android") apply false
     id("com.android.application") apply false
     id("com.android.library") apply false
-
     id("org.jetbrains.dokka") apply false
 }
 
